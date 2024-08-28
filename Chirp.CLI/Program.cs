4fea090149c1307864357be3a7e5252a2de9fc8a
OmarSemou <omse@itu.dk>
2024-08-28 15:43:14 +0200

    Implemented the read method for cheeps
    Cheeps from csv file is now displayed when read method is called
    
    Co-authored-by: Lukas Stryg <luss@itu.dk>
    
    Co-authored-by: Bruno Martinovic <brma@itu.dk>
    
    Co-authored-by: andsji <aing@itu.dk>
    
    Co-authored-by: Asger124 <asjo@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+if (args.Length < 1) 
+{  
+    Console.WriteLine("How to use the program: \n" + 
+                    " dotnet run -- <read> (Displays all cheeps in DB) \n" + 
+                    " dotnet run -- <cheep> [message] (Cheep a message to the DB)");
+}
+
+if (args[0] == "read")
+{
+    Read();
+}
+
+void Read()
+{
+    string[] lines = File.ReadAllLines("chirp_cli_db.csv");
+
+    for (int i = 1; i < lines.Length; i++)
+    {
+        string[] cheepData = lines[i].Split('"');
+
+        string author = cheepData[0].TrimEnd(',');
+        string message = cheepData[1];
+        string timestamp = cheepData[2].TrimStart(',');
+
+        string dateTime = FromUnixTimeToDateTime(timestamp);
+
+        string cheep = $"{author} @ {dateTime} : {message}";
+
+        Console.WriteLine(cheep);
+    }
+
+    string FromUnixTimeToDateTime(string timestamp)
+    {
+        int timestampConverted = int.Parse(timestamp);
+        DateTimeOffset dto = DateTimeOffset.FromUnixTimeSeconds(timestampConverted);
+        string correctFormatTimestamp = dto.ToLocalTime().ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture);
+        return correctFormatTimestamp;
+    }
+}
