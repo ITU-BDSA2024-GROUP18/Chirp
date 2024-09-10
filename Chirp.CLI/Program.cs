498333120ab97ae7f1d37aa573c2dba259599e30
Anders Juul Ingerslev <aing@itu.dk>
2024-09-10 13:47:54 +0200

    Created CSVDatabase, moved code there and refactored a bit. Not functional.

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Globalization;
 using CsvHelper;
-using SimpleDB;
+using SimpleDb;
 
 string timeFormat = "MM/dd/yy HH:mm:ss";
 
@@ -21,49 +21,5 @@ else if (args[0]  == "cheep")
     {
         Console.WriteLine("You are missing a message to cheep :(");
     }
-    Cheep(args[1]);
+    Store(args[1]);
 }
-
-
-
-void Read()
-{
-    using (var reader = new StreamReader("chirp_cli_db.csv"))
-    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-    {
-        csv.GetRecords<Cheep>();     
-        //For printing to terminal:
-        foreach (var record in csv.GetRecords<Cheep>())
-                {
-                    Console.WriteLine($"{record.Author} @ {FromUnixTimeToDateTime(record.Timestamp)} : {record.Message}");
-                }  
-    }
-}
-
-
-void Cheep(string message)
-{
-    using (var writer = new StreamWriter("chirp_cli_db.csv"))
-    using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-    {
-        Cheep cheep = new Cheep(Environment.UserName, message, 
-                      FromDateTimeToUnix(DateTime.Now.ToString(timeFormat, CultureInfo.InvariantCulture)));
-
-        csv.WriteRecord(cheep);
-    }
-}
-
-string FromUnixTimeToDateTime(long timestamp)
-{
-    DateTimeOffset dto = DateTimeOffset.FromUnixTimeSeconds(timestamp);
-    string correctFormatTimestamp = dto.ToLocalTime().ToString(timeFormat, CultureInfo.InvariantCulture);
-    return correctFormatTimestamp;
-}
-
-long FromDateTimeToUnix(string dateTimeStamp)
-{
-    DateTime parsedTime = DateTime.Parse(dateTimeStamp, CultureInfo.InvariantCulture);
-    return new DateTimeOffset(parsedTime).ToUnixTimeSeconds();
-}
-
-public record Cheep(string Author, string Message, long Timestamp);
