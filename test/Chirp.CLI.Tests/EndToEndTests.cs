70194d14c8451f69422597eb3258a289dc98875b
OmarSemou <omse@itu.dk>
2024-09-18 15:38:22 +0200

    Added end to end test for reading with limit

---
+++ test/Chirp.CLI.Tests/EndToEndTests.cs
@@ -0,0 +1,68 @@
+using Xunit;
+using System.IO;
+using System;
+using SimpleDb;
+using Chirp.CLI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+public class ChirpCliTests
+{
+    private const string TestCsvPath = "test_chirp_cli_db.csv";
+    private const string TimeFormat = "MM/dd/yy HH:mm:ss";
+
+    public ChirpCliTests()
+    {
+        CreateTestCsv(); 
+    }
+
+    [Fact]
+    public void TestReadWithLimit()
+    {
+        // Arrange
+        var csvDatabase = new CSVDatabase<Cheep>(TestCsvPath);
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter); // Redirect console output to StringWriter
+
+        // Act
+        UserInterface.PrintCheeps(csvDatabase.Read(1)); 
+
+        // Assert
+        var output = stringWriter.ToString().Trim();
+        Assert.Contains("Omar @ 01/01/24 00:00:00: Message 1", output); 
+        Assert.DoesNotContain("Asger", output); 
+
+        // Cleanup
+        Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+        stringWriter.Dispose();
+    }
+
+    private void CreateTestCsv()
+    {
+        var cheeps = new List<Cheep>
+        {
+            new Cheep("Omar", "Message 1", ConvertToUnixTimestamp("01/01/24 00:00:00")),
+            new Cheep("Asger", "Message 2", ConvertToUnixTimestamp("01/02/24 00:00:00")),
+            new Cheep("Lukas", "Message 3", ConvertToUnixTimestamp("01/03/24 00:00:00"))
+        };
+
+        using (var writer = new StreamWriter(File.Open(TestCsvPath, FileMode.Create)))
+        using (var csv = new CsvHelper.CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(cheeps); 
+        }
+    }
+
+    private long ConvertToUnixTimestamp(string dateTimeStr)
+    {
+        var dateTime = DateTime.ParseExact(dateTimeStr, TimeFormat, CultureInfo.InvariantCulture);
+        return ((DateTimeOffset)dateTime).ToUnixTimeSeconds();
+    }
+
+    [Fact]
+    public void Dispose()
+    {
+        File.Delete(TestCsvPath); 
+    }
+}
