554f6d488720af6dacf1a0574bf13dde82ea4040
Asger124 <asjo@itu.dk>
2024-10-16 14:09:11 +0200

    Added ChirpDBContext class. This addition is to give EF core context on our database. We will use it later down the road

--- src/Chirp.Razor/ChirpDBContext.cs
+++ src/Chirp.Razor/ChirpDBContext.cs
@@ -1,10 +1,20 @@
 using Microsoft.EntityFrameworkCore;
-
 namespace Chirp.Razor
 {
 
     public class ChirpDBContext : DbContext
     {
+
+        public required DbSet<Author> Authors;
+
+        public required DbSet<Cheep> cheeps;
+
+
+        public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options)
+        {
+
+
+        }
 
     }
 
