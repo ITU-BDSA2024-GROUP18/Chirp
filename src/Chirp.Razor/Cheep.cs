d98e16f1347f47c3067c55f43bf99a1e4362deab
Asger124 <asjo@itu.dk>
2024-10-22 16:54:54 +0200

    Made DTO class

--- src/Chirp.Razor/Cheep.cs
+++ src/Chirp.Razor/Cheep.cs
@@ -3,11 +3,11 @@ namespace Chirp.Razor
 
     public class Cheep
     {
-        public required int CheepID { get; set; }
-        public required int AuthorID { get; set; }
+        public required int CheepId { get; set; }
+        public required int AuthorId { get; set; }
         public required Author Author { get; set; }
         public required string Text { get; set; }
-        public DateTime Timestamp { get; set; }
+        public DateTime TimeStamp { get; set; }
     }
 
 }
