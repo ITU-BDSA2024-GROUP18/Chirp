b98707fc306e3d85847c1a69bca9e6029fe9857f
Asger124 <asjo@itu.dk>
2024-11-01 09:17:00 +0100

    General clean up Deleted files that we no longer need in our project. In the Class DBfacade the helper method for converting, unix time to datetime resided. We still had one test that used the DBfacade class to call and test this static method. As we are moving on from the facade class, we can also trash the method, as we right now dont need to convert unix to datetime, we only need to parse datetime as a string. If needed again the method is stored through a comment, as is the test.

--- src/Chirp.Infrastructure/Services/CheepService.cs
+++ src/Chirp.Infrastructure/Services/CheepService.cs
@@ -7,9 +7,6 @@ using Microsoft.Data.Sqlite;
 //always define namespace everywhere except for Program.cs
 namespace Chirp.Infrastructure.Services;
 
-//ViewModel obsolete - only used in db.facade which our tests still uses.
-public record CheepViewModel(string Author, string Message, string Timestamp);
-
 public interface ICheepService
 {
     public Task<List<CheepDTO>> GetCheeps(int pageNum);
@@ -33,9 +30,6 @@ public class CheepService : ICheepService
 
     private readonly ICheepRepository _CheepRepository;
 
-    // private int pageLimit = 32;
-
-    // private DbFacade db = new DbFacade();
 
     public CheepService(ICheepRepository CheepRepository)
     {
