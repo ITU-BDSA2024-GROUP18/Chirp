c2eb719bd1c7218042a7aa2fcf0b1ba1c9842a7d
Br00nac <brma@itu.dk>
2024-11-22 15:11:17 +0100

    Add required tags in ChirpDBContext
    Added required tags to Authors and Cheeps DbSet fields in ChirpDBContext. This is to avoid a warning
    in the constructor, since Authors and Cheeps are both non nullable, and the constructor alone does not
    instantiate them.

--- src/Chirp.Infrastructure/Data/ChirpDBContext.cs
+++ src/Chirp.Infrastructure/Data/ChirpDBContext.cs
@@ -8,9 +8,9 @@ namespace Chirp.Infrastructure.Data
     public class ChirpDBContext : IdentityDbContext<Author>
     {
 
-        public DbSet<Author> Authors { get; set; }
+        public required DbSet<Author> Authors { get; set; }
 
-        public DbSet<Cheep> Cheeps { get; set; }
+        public required DbSet<Cheep> Cheeps { get; set; }
 
         public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options)
         {
