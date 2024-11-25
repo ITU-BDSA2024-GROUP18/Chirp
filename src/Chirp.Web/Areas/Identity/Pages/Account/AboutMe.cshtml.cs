c1ce9fa072e1ce8cfb6ec27fbfcbe50281c7335e
OmarSemou <omse@itu.dk>
2024-11-25 14:49:51 +0100

    Implemented forget me feature (deletes user and user's cheeps from db)

--- src/Chirp.Web/Areas/Identity/Pages/Account/AboutMe.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/AboutMe.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Security.Claims;
 using Chirp.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Chirp.Web.Areas.Identity.Pages.Account
 {
@@ -38,6 +40,32 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             UserCheeps = _dbContext.Cheeps.Where(c => c.AuthorId == userId).ToList();
 
             // TODO: Get following list 
+        }
+
+        public async Task<IActionResult> OnPostForgetMeAsync()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId)) return BadRequest();
+
+            // Fetch the user from the database
+            var user = await _dbContext.Users.FindAsync(userId);
+            if (user == null) return NotFound();
+
+            // Delete user's cheeps
+            var userCheeps = _dbContext.Cheeps.Where(c => c.AuthorId == userId).ToList();
+            _dbContext.Cheeps.RemoveRange(userCheeps);
+
+            // Delete the user account
+            _dbContext.Users.Remove(user);
+
+            // Save changes
+            await _dbContext.SaveChangesAsync();
+
+            // Log the user out
+            await HttpContext.SignOutAsync();
+
+            // Redirect to Public Timeline
+            return RedirectToPage("/Account/Logout", new { area = "Identity", returnUrl = "/Public" });
         }
     }
 }
